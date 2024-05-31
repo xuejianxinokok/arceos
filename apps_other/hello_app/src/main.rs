db59392d0ceaf6db2ffcf1fe4ec661b3d6848a7d
xuejianxinokok <xuejianxinokok@163.com>
2024-05-31 06:32:22 +0000

    实验4：正式在 App 中调用 ABI

--- apps_other/hello_app/src/main.rs
+++ apps_other/hello_app/src/main.rs
@@ -35,7 +35,7 @@ unsafe extern "C" fn _start(){
     // syscall::putchar('\n');
 
     // 不知道为为什么这个循环不行
-    // syscall::puts("LAB4 PUTS TEST! \n");
+    syscall::puts("LAB4 PUTS TEST! \n");
     syscall::exit(0);
     
 
